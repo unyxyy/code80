6db3e743b288313b748723659130c9e14cbf1d08
unyxyy <stasknazev15@gmail.com>
2025-12-02 12:40:58 +0300

    Update code80.py

--- code80.py
+++ code80.py
@@ -1,11 +1,20 @@
 from prettytable import PrettyTable
+import matplotlib.pyplot as diagram
 
 receipt = PrettyTable()
 receipt.field_names = ['№','Продукт','Цена','Количество','Стоимость']
+
+products = []
+pay = []
 
 for i in range(3):
   product = input('Укажите название продукта: ')
   price = int(input('Введите цену продукта: '))
   quantity = int(input('Укажите количество продукта: '))
   receipt.add_row([i+1, product, price, quantity, price*quantity])
-print(receipt)
+  products.append(product)
+  pay.append(price*quantity)
+
+  print(receipt)
+  diagram.bar(products, pay)
+  diagram.show()
